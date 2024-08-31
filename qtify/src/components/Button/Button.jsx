9992806bd2d8f905a0c41c723745f70a08c10976
Dhawal Sarin <dhawal12sarin@gmail.com>
2024-08-31 19:06:49 +0530

    Button css Changes.

--- qtify/src/components/Button/Button.jsx
+++ qtify/src/components/Button/Button.jsx
@@ -3,7 +3,7 @@ import styles from "./Button.module.css";
 export default function Button(props){
     return(
         <button className={styles.FeedbackBtn}>
-            <span className={styles.text}>{props.children}</span>
+            {props.children}
         </button>
     )
 }
